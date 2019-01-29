995810179861c62a785f8e18350864195ac1d586
Noah Geraci <noah.geraci@ucr.edu>
2019-01-28 17:52:44 -0800

    regex filtering on what LC terms to include

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 An exploration of using labels from Wikidata to replace Library of Congress Subject Heading terms that name indigenous peoples in descriptive metadata. 
 
-1. Use Library of Congress linked data API to recursively get all narrower terms for "Indians of North America" and write to CSV file ("narrower_terms.csv")
+1. Use Library of Congress linked data API to recursively get all narrower terms for "Indians of North America" and write to CSV file ("narrower_terms.csv"). Use a regular expression to mark labels for inclusion that contain the word "Indians" and refer to overarching groups of people. (get_lc_terms.py)
 
-2. Manually review CSV file to determine which terms are names of peoples or tribes.
+[include a note about not dealing with "Eskimo" term due to taxonomy issues, not dealing with sub-terms like "Older Navajo Indians", not dealing with terms like "Pima baskets", which is an NT of "Baskets--Arizona"]
+
+2. Manually review CSV file.
--- get_lc_terms.py
+++ get_lc_terms.py
@@ -1,13 +1,16 @@
 """ Uses Library of Congress linked data API to recursively
 get all narrower terms of a given subject heading URI and
 write them to a CSV file called "narrower_terms.csv"
-with columns "URI" and "Label".
+with columns "URI", "Label", and "Include".
 
 """
+import re
 import csv
 import requests
 from bs4 import BeautifulSoup
 
+# a regular expression to match the labels we want
+LABEL_RE = re.compile(r"^(?!Older).* Indians( \(.*\))?$")
 
 def main():
     """
@@ -17,7 +20,7 @@ def main():
     outfile = "narrower_terms.csv"
     with open(outfile, "w", newline="") as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(["URI", "Label"])
+        writer.writerow(["URI", "Label", "Include"])
 
     get_narrower_terms_recursive(
         "http://id.loc.gov/authorities/subjects/sh85065184", outfile
@@ -37,7 +40,11 @@ def get_narrower_terms_recursive(subject_uri, outfile):
             authority = term.find("madsrdf:authority")
             uri = authority["rdf:about"]
             label = authority.find("madsrdf:authoritativelabel").text
-            writer.writerow([uri, label])
+            if re.match(LABEL_RE, label):
+                include = "True"
+            else:
+                include = "False"
+            writer.writerow([uri, label, include])
             get_narrower_terms_recursive(uri, outfile)
 
 
