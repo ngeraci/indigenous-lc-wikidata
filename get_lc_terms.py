822f2d57e0d783fb4ed6153e2534eab548604888
Noah Geraci <ngeraci@protonmail.com>
2019-02-10 00:52:26 -0800

    basically good i think??

--- get_lc_terms.py
+++ get_lc_terms.py
@@ -1,54 +1,51 @@
-""" Uses Library of Congress linked data API to recursively
-get all narrower terms of a given subject heading URI and
-write them to a CSV file called 'narrower_terms.csv'
-with columns 'URI', 'Label', and 'Include'.
+"""
 
 """
-import re
 import csv
+import re
 from urllib.parse import quote
 import requests
-import pandas as pd
 from bs4 import BeautifulSoup
-
-# a regular expression to match the labels we want
-LABEL_RE = re.compile(r'^(?!Older).* Indians( \(.*\))?$')
+from SPARQLWrapper import SPARQLWrapper, JSON
 
 def main():
-    """
-    Write CSV headers and call term-getting functions.
-    """
 
-    outfile = 'lcsh.csv'
-    with open(outfile, 'w', encoding='utf-8', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(['URI', 'Label'])
+    dataset = []
 
-    get_narrower_terms_recursive(
-        'http://id.loc.gov/authorities/subjects/sh85065184', outfile
-    )
+    primary_uri = "http://id.loc.gov/authorities/subjects/sh85065184"
+    childrens_uri = "http://id.loc.gov/authorities/childrensSubjects/sj96005727"
 
-    compound_term_uris = get_compound_terms('http://id.loc.gov/authorities/childrensSubjects/sj96005727')
-    for uri in compound_term_uris:
-        get_narrower_terms_recursive(uri, outfile)
+    get_narrower_terms_recursive(primary_uri, dataset)
+
+    compound_subject_uris = get_compound_terms(childrens_uri)
+    for uri in compound_subject_uris:
+        get_narrower_terms_recursive(uri, dataset)
+
+    dataset = tidy(dataset)
+
+    write_out(dataset, "subject_headings.csv")
 
 
-def get_narrower_terms_recursive(subject_uri, outfile):
+def get_narrower_terms_recursive(subject_uri, dataset):
 
     rdf = requests.get(subject_uri + '.rdf').text
     soup = BeautifulSoup(rdf, features="html.parser")
+    label_re = re.compile(r'^(?!Older).* Indians( \(.*\))?$')
 
     narrower_terms = soup.find_all('madsrdf:hasnarrowerauthority')
 
-    with open(outfile, 'a', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        for term in narrower_terms:
-            authority = term.find('madsrdf:authority')
-            uri = authority['rdf:about']
-            label = authority.find('madsrdf:authoritativelabel').text
-            if re.match(LABEL_RE, label):
-                writer.writerow([uri, label])
-            get_narrower_terms_recursive(uri, outfile)
+    for term in narrower_terms:
+        authority = term.find('madsrdf:authority')
+        lc_uri = authority['rdf:about']
+        lc_label = authority.find('madsrdf:authoritativelabel').text
+        if re.match(label_re, lc_label):
+            wikidata = get_wikidata(lc_uri)
+            dataset.append({"LCSH Label": lc_label,
+                              "LCSH URI": lc_uri,
+                              "Wikidata Label": wikidata["label"],
+                              "Wikidata URI": wikidata["uri"]})
+
+        get_narrower_terms_recursive(lc_uri, dataset)
 
 
 def get_compound_terms(subject_uri):
@@ -107,6 +104,51 @@ def uris_from_search_urls(search_urls):
 
     return complex_subject_uris
 
+
+def get_wikidata(lc_uri):
+
+    lc_identifier = lc_uri.split("/")[5]
+
+    # initialize sparql wrapper
+    sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
+
+    query_parts = []
+    select_where = "SELECT ?item ?itemLabel WHERE {"
+    # wdt:P244 is the property for LC Authority URI
+    query = '?item wdt:P244 "{}".'.format(lc_identifier)
+    language = 'SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }'
+    closing = "}"
+    for part in [select_where, query, language, closing]:
+        query_parts.append(part)
+    request = "\n".join(query_parts)
+    sparql.setQuery(request)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+
+    wikidata_uri = ";".join([item["item"]["value"] for item in results["results"]["bindings"]])
+    wikidata_label = ";".join([item["itemLabel"]["value"] for item in results["results"]["bindings"]])
+
+    return {"uri": wikidata_uri, "label": wikidata_label}
+
+def tidy(dataset):
+    """ Remove duplicate rows
+        Sort by LCSH Label
+    """
+    ## https://stackoverflow.com/questions/9427163/remove-duplicate-dict-in-list-in-python
+    dataset = [dict(t) for t in {tuple(d.items()) for d in dataset}]
+
+    dataset = sorted(dataset, key=lambda k: k['LCSH Label'])
+
+    return dataset
+
+def write_out(dataset, outfile):
+    """ List of dicts to CSV file
+    """
+    with open(outfile, "w", encoding="utf-8", newline="") as csv_out:
+        writer = csv.DictWriter(csv_out, fieldnames=dataset[0].keys())
+        writer.writeheader()
+        for row in dataset:
+            writer.writerow(row)
 
 if __name__ == '__main__':
     main()
